45852a2b632075cf8ee764aad25c81316229368c
nisarbasha <nisarbasha1993@gmail.com>
2022-05-11 17:28:57 +0530

    Initial commit

---
+++ azuerusercredential/userdetail/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('signin', sign_in, name='signin'),
+    path('landingpage', landingpage),
+path('signout', sign_out, name='signout'),
+    ]
