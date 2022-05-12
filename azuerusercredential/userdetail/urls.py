e0eee399d55ef7a6011e1e45266475c4d00d92e4
nisarbasha <nisarbasha1993@gmail.com>
2022-05-12 10:51:29 +0530

    Done

--- azuerusercredential/userdetail/urls.py
+++ azuerusercredential/userdetail/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('signin', sign_in, name='signin'),
     path('landingpage', landingpage),
-path('signout', sign_out, name='signout'),
+    path('signout', sign_out, name='signout'),
+    path('calendar', calendar, name='calendar'),
+    path('calendar/new', newevent, name='newevent'),
     ]
--- azuerusercredential/userdetail/views.py
+++ azuerusercredential/userdetail/views.py
@@ -1,5 +1,8 @@
+from datetime import timedelta, datetime
+from dateutil import tz, parser
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 from django.urls import reverse
 
 from .auth_helper import *
@@ -49,14 +52,106 @@ def landingpage(request):
 
     # Get the user's profile
     user = get_user(result['access_token'])
-
+    print(user['displayName'])
     # Store user
     store_user(request, user)
     return HttpResponseRedirect(reverse('home'))
 
 
-def sign_out(request):
-  # Clear out the user and token
-  remove_user_and_token(request)
+def user_profile(request):
+    context = initialize_context(request)
+    context['subscriptionId'] = request.session.get('subscriptionId')
+    return render(request, 'user_profile.html', context)
 
-  return HttpResponseRedirect(reverse('home'))
+
+def sign_out(request):
+    # Clear out the user and token
+    remove_user_and_token(request)
+
+    return HttpResponseRedirect(reverse('home'))
+
+
+def calendar(request):
+  context = initialize_context(request)
+  user = context['user']
+
+  # Load the user's time zone
+  # Microsoft Graph can return the user's time zone as either
+  # a Windows time zone name or an IANA time zone identifier
+  # Python datetime requires IANA, so convert Windows to IANA
+  time_zone = get_iana_from_windows(user['timeZone'])
+  tz_info = tz.gettz(time_zone)
+
+  # Get midnight today in user's time zone
+  today = datetime.now(tz_info).replace(
+    hour=0,
+    minute=0,
+    second=0,
+    microsecond=0)
+
+  # Based on today, get the start of the week (Sunday)
+  if (today.weekday() != 6):
+    start = today - timedelta(days=today.isoweekday())
+  else:
+    start = today
+
+  end = start + timedelta(days=7)
+
+  token = get_token(request)
+
+  events = get_calendar_events(
+    token,
+    start.isoformat(timespec='seconds'),
+    end.isoformat(timespec='seconds'),
+    user['timeZone'])
+
+  if events:
+    # Convert the ISO 8601 date times to a datetime object
+    # This allows the Django template to format the value nicely
+    for event in events['value']:
+      event['start']['dateTime'] = parser.parse(event['start']['dateTime'])
+      event['end']['dateTime'] = parser.parse(event['end']['dateTime'])
+
+    context['events'] = events['value']
+
+  return render(request, 'calendar.html', context)
+
+
+def newevent(request):
+  context = initialize_context(request)
+  user = context['user']
+
+  if request.method == 'POST':
+    # Validate the form values
+    # Required values
+    if (not request.POST['ev-subject']) or \
+       (not request.POST['ev-start']) or \
+       (not request.POST['ev-end']):
+      context['errors'] = [
+        { 'message': 'Invalid values', 'debug': 'The subject, start, and end fields are required.'}
+      ]
+      return render(request, 'newevent.html', context)
+
+    attendees = None
+    if request.POST['ev-attendees']:
+      attendees = request.POST['ev-attendees'].split(';')
+    body = request.POST['ev-body']
+
+    # Create the event
+    token = get_token(request)
+
+    create_event(
+      token,
+      request.POST['ev-subject'],
+      request.POST['ev-start'],
+      request.POST['ev-end'],
+      attendees,
+      request.POST['ev-body'],
+      user['timeZone'])
+
+    # Redirect back to calendar view
+    return HttpResponseRedirect(reverse('calendar'))
+  else:
+    # Render the form
+    return render(request, 'newevent.html', context)
+
